cac657c56b0216d6557e944383ad901177bc86d6
Luis Miguel Huerta <lmiguelhv@gmail.com>
2025-01-07 22:27:08 -0600

    First commit

---
+++ backend/lambda_function/requirements.txt
@@ -0,0 +1,1 @@
+# aws-hola-mundo-test
---
+++ frontend/pages/index.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('<API_ENDPOINT>')
+      .then(response => response.json())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Microservicio Hola Mundo</h1>
+      <p>Respuesta del API: {message}</p>
+    </div>
+  );
+}
