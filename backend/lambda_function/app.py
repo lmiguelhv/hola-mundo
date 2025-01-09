98d16f8e15216061ccdbf6c7afd73f26c7048719
Luis Miguel Huerta <lmiguelhv@gmail.com>
2025-01-09 12:09:49 -0600

    Add GitHub Actions workflow for dev branch deployment

--- backend/lambda_function/app.py
+++ backend/lambda_function/app.py
@@ -4,5 +4,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hola Mundo! + 1 + 2')
+        'body': json.dumps('Hola Mundo! + 1 + 2 + Foo Bar')
     }
